b7bd2625f2e14114fecaeb035123246abe199b89
Lewis <tawerlewis@gmail.com>
2025-03-11 18:45:10 +0500

    Создание моделей категорий и продуктов. Шайхилисламов

---
+++ Classes/item_categories.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerShop.Classes
+{
+    public class item_categories
+    {
+        public int CategoryID { get; set; }
+        public string CategoriesName { get; set; }
+        public string Src { get; set; }
+
+        public item_categories(int categoryID, string categoriesName, string src)
+        {
+            CategoryID = categoryID;
+            CategoriesName = categoriesName;
+            Src = src;
+        }
+    }
+}
---
+++ Classes/item_products.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerShop.Classes
+{
+    public class item_products
+    {
+        public int ProductID { get; set; }
+        public item_categories Category { get; set; }
+        public string ProductsName { get; set; }
+        public int ProductsPrice { get; set; }
+        public string Src { get; set; }
+
+        public item_products(int productID, item_categories category, string productsName, int productsPrice, string src)
+        {
+            ProductID = productID;
+            Category = category;
+            ProductsName = productsName;
+            ProductsPrice = productsPrice;
+            Src = src;
+        }
+    }
+}
