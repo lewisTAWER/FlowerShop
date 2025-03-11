dde3082932029fe1f280347f5dd51aef14e7419e
Lewis <tawerlewis@gmail.com>
2025-03-11 18:37:01 +0500

    Подключение к БД. Шайхилисламов

---
+++ Classes/Common/DBConnection.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerShop.Classes.Common
+{
+    public class DBConnection
+    {
+        public static readonly string config = "server=127.0.0.1;port=3306;;uid=root;pwd=;database=FlowerShop;";
+
+        public static MySqlConnection OpenConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(config);
+            connection.Open();
+
+            return connection;
+        }
+
+        public static MySqlDataReader Query(string SQL, MySqlConnection connection)
+        {
+            return new MySqlCommand(SQL, connection).ExecuteReader();
+        }
+
+        public static void CloseConnection(MySqlConnection connection)
+        {
+            connection.Close();
+            MySqlConnection.ClearPool(connection);
+        }
+    }
+}
